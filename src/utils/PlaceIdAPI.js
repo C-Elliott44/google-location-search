46bb4315b8621433e69b0c06a8716335d56a1131
C-Elliott44 <cameronjohnelliott@gmail.com>
2019-02-13 20:10:05 -0800

    took out some comments

--- src/utils/PlaceIdAPI.js
+++ src/utils/PlaceIdAPI.js
@@ -4,7 +4,6 @@ const BASEURL = "https://maps.googleapis.com/maps/api/place/findplacefromtext/js
 const FIELDS = "&inputtype=textquery&fields=place_id";
 const APIKEY = "&key=AIzaSyDsxlxWsTHrU0nPpFDIqVpJMq6yGG7SrLw";
 
-// Export an object with a "search" method that searches the Giphy API for the passed query
 export default {
   search: function(query) {
     return axios.get(`${'https://cors-anywhere.herokuapp.com/'}` + BASEURL + query + FIELDS + APIKEY);
